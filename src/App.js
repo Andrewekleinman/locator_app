863600f40d528bb286166f11dd682e5ce593e0d2
Andrew Kleinman <andrewekleinman@gmail.com>
2021-11-15 13:03:28 -0800

    map links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
+import xbutton from './xbutton.png'
 import './App.css';
 import React from "react";
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+
 
 class App extends React.Component {
     state = {type: 1, prev: 1, current: 0}
@@ -25,9 +28,7 @@ class App extends React.Component {
     }
 
     getName(i) {
-        // console.log(this.get(i).name)
         return this.get(i).name
-
     }
 
     getUrl(i) {
@@ -49,6 +50,36 @@ class App extends React.Component {
     getPostal(i) {
         return this.get(i).postal_code
     }
+    getMonday(i) {
+        return  this.get(i).monday_open + " - " + this.get(i).monday_close
+    }
+    getTuesday(i) {
+        return this.get(i).tuesday_open + " - " + this.get(i).tuesday_close
+    }
+    getWednesday(i) {
+        return this.get(i).wednesday_open + " - " + this.get(i).wednesday_close
+    }
+    getThursday(i) {
+        return this.get(i).thursday_open + " - " + this.get(i).thursday_close
+    }
+    getFriday(i) {
+        return this.get(i).friday_open + " - " + this.get(i).friday_close
+    }
+    getSaturday(i) {
+        return this.get(i).saturday_open + " - " + this.get(i).saturday_close
+    }
+    getSunday(i) {
+        return this.get(i).sunday_open + " - " + this.get(i).sunday_close
+    }
+    getLink(i) {
+        return this.get(i).url
+    }
+    getLat(i) {
+        return this.get(i).latitude
+    }
+    getLong(i) {
+        return this.get(i).longitude
+    }
 
     getRightPanel(i) {
         if (this.state.type === 1) {
@@ -60,48 +91,75 @@ class App extends React.Component {
         } else if (this.state.type === 2) return (
             <div className="Right">
                 <div className="Map">
-                    not testing a map panel
+                    <Map google={this.props.google} />
                 </div>
             </div>
         )
-        else {
-            // console.log(this.state.current);
+        else if(this.state.type ===3) {
             return (
                 <div className="Right">
-                    <div id="overlay" onClick={() => this.setState({type: this.state.prev, prev: 3})}>
-                        <div style={{textAlign: "center"}}><img className="centerImage" width={200} src={logo}
-                                                                alt={logo}></img></div>
-                        <div style={{fontSize: 25, marginLeft: 10}}>
-                            {this.getName(this.state.current)}
-                        </div>
-                        <br/>
-                        <div style={{marginLeft: 10}}>
-                            {this.getAddress(this.state.current)}
+                    <div id="overlay" >
+                        <div id = "overlay2">
+                            <div style={{textAlign: "right"}}><img onClick={() => this.setState({type: this.state.prev, prev: 3})} className="XButton" width={45} src={xbutton} alt={logo}></img></div>
+                            <div style={{textAlign: "center"}}><img className="centerImage" width={200} src={logo} alt={logo}></img></div>
+                            <br/><br/>
+                            <div style={{fontSize: 25, marginLeft: 10}}>
+                                {this.getName(this.state.current)}
+                            </div>
                             <br/>
-                            {this.getCity(this.state.current)}, {this.getState(this.state.current)} {this.getPostal(this.state.current)}
+                            <div style={{marginLeft: 10}}>
+                                {this.getAddress(this.state.current)}
+                                <br/>
+                                {this.getCity(this.state.current)}, {this.getState(this.state.current)} {this.getPostal(this.state.current)}
+                            </div>
+                            <br/><br/>
+                            <div style={{marginLeft: 10 ,textAlign:"center"}}>
+                                Monday: &emsp;&emsp; {this.getMonday(this.state.current)}
+                                <br/>
+                                Tuesday: &emsp;&ensp;&nbsp; {this.getTuesday(this.state.current)}
+                                <br/>
+                                Wednesday: &nbsp; {this.getWednesday(this.state.current)}
+                                <br/>
+                                Thursday: &emsp;&nbsp; {this.getThursday(this.state.current)}
+                                <br/>
+                                Friday: &emsp;&emsp;&ensp;  {this.getFriday(this.state.current)}
+                                <br/>
+                                Saturday: &emsp;&ensp; {this.getSaturday(this.state.current)}
+                                <br/>
+                                Sunday: &emsp;&emsp; {this.getSunday(this.state.current)}
+                            </div>
+                            <br/><br/>
+                            <div style={{textAlign: "center"}}><button className="Button" onClick={(e) => {
+                                e.preventDefault();
+                                window.location.href=this.getLink(this.state.current);
+                            }}> VIEW FULL DETAILS </button></div>
                         </div>
-                        <br/>
                     </div>
+                </div>
+            )
+        }
+        else {
+            const showInMapClicked = () => {
+                window.open("https://www.google.com/maps/dir//"+ this.getLat(this.state.current)+","+this.getLong(this.state.current));
+            };
+            return(
+                <div className="right">
+                    {showInMapClicked()}
                 </div>
             )
         }
     }
 
-    handleClick(event) {
+    handleClickInfo = (event) => {
         const id = event.target.id;
-
-        console.log(id);
-    }
-
-    delta() {
-        this.setState({
-            type : 3,
-            prev: this.state.type,
-            current: 1
-        });
+        var prev = (this.state.type !== 3? this.state.type:this.state.prev)
+        this.setState({type: 3, prev, current: id})
     }
 
     render() {
+        const showInMapClicked = () => {
+            window.open("https://www.google.com/maps/dir//"+ this.getLat(this.state.current)+","+this.getLong(this.state.current));
+        };
         if (this.obj === -1) {
             this.request()
         }
@@ -125,13 +183,11 @@ class App extends React.Component {
                     </div>
                     <div className="App" style={{marginLeft: 7.5}}>
                         <button id={i} className="Button"
-                                onClick={() => this.setState({type: 1, prev: this.state.type})}>DIRECTIONS
+                                onClick={showInMapClicked}>DIRECTIONS
                         </button>
                         <button id={i} className="Button"
-                                onClick={() => this.setState({type: 3, prev: this.state.type})}>MORE INFO
+                                onClick={this.handleClickInfo}>MORE INFO
                         </button>
-                        {/*<button id={i} onClick={this.handleClick}>Button</button>*/}
-                        <button id="testID" onClick={this.delta.bind(this)}>+</button>
                     </div>
                 </div>
             )
@@ -148,5 +204,6 @@ class App extends React.Component {
     }
 
 }
+
 
 export default App;
